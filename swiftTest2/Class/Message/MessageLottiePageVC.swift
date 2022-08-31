6b582cf38893f2252381f5818a28db1eb3c30e12
zhengyachao <15038253754@163.com>
2022-08-31 17:38:35 +0800

    添加gcg定时器和自定义圆形进度条。

--- swiftTest2/Class/Message/MessageLottiePageVC.swift
+++ swiftTest2/Class/Message/MessageLottiePageVC.swift
@@ -9,24 +9,41 @@ import UIKit
 import Lottie
 
 class MessageLottiePageVC: YCBaseViewController {
+    //gcd计时器
+    var gcdTimer : DispatchSourceTimer?
+    var count : Int = 60
+    
+    lazy var progressView: YCCircleProgressView = {
+        let progressView = YCCircleProgressView()
+        progressView.ratio = 0.0
+        progressView.backLineWidth = 2.0
+        progressView.backLineColor = .background
+        progressView.progressWidth = 2.0
+        progressView.progressColor = .theme
+        progressView.backLineColor = .white
+        
+        return progressView
+    }()
     
     lazy var lottieChickenView: AnimationView = {
         let lottieChickenView = AnimationView(name: "chicken")
         // 到后台时AnimationView的行为
         lottieChickenView.backgroundBehavior = .stop
         // 循环模式
-        lottieChickenView.loopMode = .playOnce
+        lottieChickenView.loopMode = .loop
         return lottieChickenView
     }()
     
     lazy var lottieMouseView: AnimationView = {
         let lottieMouseView = AnimationView(name: "mouse")
-        //
+        // 循环模式
+        lottieChickenView.loopMode = .loop
         return lottieMouseView
     }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "定时器+Lottie动画"
 
         // Do any additional setup after loading the view.
         view.addSubview(lottieChickenView)
@@ -44,11 +61,75 @@ class MessageLottiePageVC: YCBaseViewController {
             make.height.equalTo(200)
         }
         
+        /*
         lottieChickenView.play { (isFinished) in
             self.lottieChickenView.stop()
             self.lottieChickenView.removeFromSuperview()
             self.lottieMouseView.isHidden = false
             self.lottieMouseView.play()
         }
+        */
+        
+        view.addSubview(self.progressView)
+        self.progressView.snp.makeConstraints { make in
+            make.left.top.equalTo(10)
+            make.width.height.equalTo(100)
+        }
+        
+        lottieChickenView.play()
+        
+        createGCDTimer()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+//        cancelGcdTimer()
+    }
+    // 创建定时器
+    func createGCDTimer () {
+        // 创建定时器
+        self.gcdTimer = DispatchSource.makeTimerSource(flags: .strict, queue: DispatchQueue.global())
+        /*
+         dealine: 开始执行时间
+         repeating: 重复时间间隔
+         leeway: 时间精度
+         */
+        self.gcdTimer?.schedule(deadline: .now() + .seconds(1), repeating: .seconds(1))
+        // 定时器触发 监听定时器的回调
+        self.gcdTimer?.setEventHandler(handler: { [weak self] in
+            // 主线程
+            DispatchQueue.main.async {
+                self?.count -= 1
+                self?.progressView.ratio += 1/60
+                self?.progressView.textLabel.text = String(self!.count) + "秒"
+                self?.progressView.textLabel.font = UIFont.systemFont(ofSize: 12)
+                print("times---",self?.count as Any)
+                if self?.count == 0 {
+                    // 取消定时器
+                    self?.cancelGcdTimer()
+                    // 停止动画
+                    self?.lottieChickenView.stop()
+                    // 隐藏圆形进度条
+                    self?.progressView.isHidden = true
+                }
+            }
+        })
+        self.gcdTimer?.resume()   // 开始
+        //self.gcdTimer?.suspend()// 暂停
+        //self.gcdTimer?.cancel() // 取消
+    }
+    
+    //MARK: -- 取消定时器
+    func cancelGcdTimer () {
+        if (self.gcdTimer != nil) {
+            self.gcdTimer?.cancel()
+            // 将定时器释放
+            self.gcdTimer = nil
+        }
+    }
+    // 析构过程原理（类似OC里的dealloc）
+    deinit {
+        print("deinit----")
+        cancelGcdTimer()
     }
 }
---
+++ swiftTest2/View&Cell/Custom/YCCircleProgressView/YCCircleProgressView.swift
@@ -0,0 +1,111 @@
+//
+//  YCCircleProgressView.swift
+//  swiftTest2
+//
+//  Created by trustmobi on 2022/8/31.
+//
+
+import UIKit
+
+class YCCircleProgressView: UIView {
+    
+    /// 底条颜色
+    public var backLineColor: UIColor = .lightGray
+    /// 底条宽度
+    public var backLineWidth: CGFloat = 6.0
+    /// 进度条颜色
+    public var progressColor: UIColor = .brown
+    /// 进度条宽度
+    public var progressWidth: CGFloat = 6.0
+    /// 进度比例（0～1）
+    public var ratio: CGFloat = 0 {
+        didSet {
+            var newValue = ratio
+            if newValue < 0 {
+                newValue = 0
+            } else if (newValue > 1) {
+                newValue = 1
+            }
+            self.lineLayer?.strokeEnd  = newValue
+        }
+    }
+    
+    /// 开始弧度
+    public var startAngle: CGFloat = CGFloat(-(Double.pi / 2))
+    
+    /// 结束弧度
+    public var endAngle: CGFloat = CGFloat(Double.pi / 2 * 3)
+    
+    /// 中间文本框
+    public lazy private(set) var textLabel: UILabel = {
+        let textLabel = UILabel()
+        textLabel.textColor = .brown
+        textLabel.textAlignment = .center
+        textLabel.font = .systemFont(ofSize: 50)
+        return textLabel
+    }()
+    
+    private var lineLayer: CAShapeLayer?
+        
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required public init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setup()
+    }
+    
+    func setup() {
+        self.backgroundColor = .clear
+        addSubview(textLabel)
+    }
+    
+    open override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        
+        let radius: CGFloat = ((self.frame.size.width > self.frame.size.height ? self.frame.size.height : self.frame.size.width)-(self.backLineWidth > self.progressWidth ? self.backLineWidth : self.progressWidth))*0.5;
+        
+        let path = UIBezierPath()
+        let centerPoint = CGPoint(x: self.bounds.size.width * 0.5, y: self.bounds.size.height * 0.5)
+        path.addArc(withCenter: centerPoint, radius: radius, startAngle: self.startAngle, endAngle: self.endAngle, clockwise: true)
+        
+        let backLayer = CAShapeLayer()
+        backLayer.frame = self.bounds
+        backLayer.fillColor = UIColor.clear.cgColor
+        backLayer.lineWidth = self.backLineWidth
+        backLayer.strokeColor = self.backLineColor.cgColor
+        backLayer.strokeStart = 0
+        backLayer.strokeEnd = 1
+        backLayer.lineCap = .round
+        backLayer.path = path.cgPath
+        self.layer.addSublayer(backLayer)
+        
+        let lineLayer = CAShapeLayer()
+        lineLayer.frame = self.bounds
+        lineLayer.fillColor = UIColor.clear.cgColor
+        lineLayer.lineWidth = self.progressWidth
+        lineLayer.strokeColor = self.progressColor.cgColor
+        lineLayer.strokeStart = 0
+        
+//        CATransition.begin()
+//        CATransition.setDisableActions(false)
+//        CATransition.setAnimationTimingFunction(CAMediaTimingFunction(name: .easeIn))
+//        CATransition.setAnimationDuration(1.0)
+        lineLayer.strokeEnd = self.ratio
+//        CATransition.commit()
+        
+        lineLayer.lineCap = .round
+        lineLayer.path = path.cgPath
+        self.layer.addSublayer(lineLayer)
+        self.lineLayer = lineLayer
+    }
+    
+    open override func layoutSubviews() {
+        super.layoutSubviews()
+        self.textLabel.frame = CGRect(x: 0, y: 0, width: self.bounds.size.width * 0.8, height: self.bounds.size.height * 0.5)
+        self.textLabel.center = CGPoint(x: self.bounds.size.width * 0.5, y: self.bounds.size.height * 0.5)
+    }
+}
+
